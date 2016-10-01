92d2c580e63f96daf4d59ea459664190897771ab
Chris Severns <severnsc@gmail.com>
2016-10-01 12:58:00 -0400

    Add form partial, nd editing action/view

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -13,9 +13,26 @@ class ArticlesController < ApplicationController
 		@article = Article.new
 	end
 
+	def edit
+		@article = Article.find(params[:id])
+	end
+
 	def create
 		@article = Article.new(article_params)
 		@article.save
 		redirect_to article_path(@article)
 	end
+
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+		redirect_to article_path(@article)
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		redirect_to articles_path
+	end
+
 end
